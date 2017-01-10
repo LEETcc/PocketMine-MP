859cdbea5f238df976b1106586857f16c480ea04
Dylan K. Taylor <odigiman@gmail.com>
2017-01-10 12:24:59 +0000

    Re-implemented Position for Dimension support

--- src/pocketmine/level/Position.php
+++ src/pocketmine/level/Position.php
@@ -21,72 +21,83 @@
 
 namespace pocketmine\level;
 
+use pocketmine\level\dimension\Dimension;
+use pocketmine\level\utils\LevelException;
 use pocketmine\math\Vector3;
-use pocketmine\utils\LevelException;
 use pocketmine\utils\MainLogger;
 
 class Position extends Vector3{
 
-	/** @var Level */
-	public $level = null;
+	/** @var Dimension */
+	protected $dimension = null;
 
 	/**
-	 * @param int   $x
-	 * @param int   $y
-	 * @param int   $z
-	 * @param Level $level
+	 * @param int|float      $x
+	 * @param int|float      $y
+	 * @param int|float      $z
+	 * @param Dimension|null $dimension
 	 */
-	public function __construct($x = 0, $y = 0, $z = 0, Level $level = null){
+	public function __construct($x = 0, $y = 0, $z = 0, Dimension $dimension = null){
 		$this->x = $x;
 		$this->y = $y;
 		$this->z = $z;
-		$this->level = $level;
+		$this->setDimension($dimension);
 	}
 
-	public static function fromObject(Vector3 $pos, Level $level = null){
-		return new Position($pos->x, $pos->y, $pos->z, $level);
+	public static function fromObject(Vector3 $pos, Dimension $dimension = null){
+		return new Position($pos->x, $pos->y, $pos->z, $dimension);
 	}
 
 	/**
-	 * Returns the target Level, or null if the target is not valid.
-	 * If a reference exists to a Level which is closed, the reference will be destroyed and null will be returned.
+	 * Returns the target Dimension, or null if the target is not valid.
+	 * If a reference exists to a Dimension which is closed or has an invalid parent, the reference will be destroyed and null will be returned.
+	 *
+	 * @return Dimension|null
+	 */
+	public function getDimension(){
+		if($this->dimension !== null and $this->dimension->isGarbage()){
+			MainLogger::getLogger()->debug("Position was holding a reference to a garbage Dimension");
+			$this->dimension = null;
+		}
+
+		return $this->dimension;
+	}
+
+	/**
+	 * Sets the target Dimension of this Position
+	 *
+	 * @param Dimension|null $dimension
+	 *
+	 * @throws \InvalidArgumentException if the specified target is not usable
+	 */
+	public function setDimension(Dimension $dimension = null){
+		if($dimension !== null and $dimension->isGarbage()){
+			throw new \InvalidArgumentException("Specified dimension has been unloaded and cannot be used");
+		}
+
+		$this->dimension = $dimension;
+	}
+
+	/**
+	 * Returns the parent Level of the target Dimension, or null if the target is not valid.
 	 *
 	 * @return Level|null
 	 */
 	public function getLevel(){
-		if($this->level !== null and $this->level->isClosed()){
-			MainLogger::getLogger()->debug("Position was holding a reference to an unloaded Level");
-			$this->level = null;
+		if($this->isValid()){
+			return $this->dimension->getLevel();
 		}
 
-		return $this->level;
+		return null;
 	}
 
 	/**
-	 * Sets the target Level of the position.
-	 *
-	 * @param Level|null $level
-	 *
-	 * @return $this
-	 *
-	 * @throws \InvalidArgumentException if the specified Level has been closed
-	 */
-	public function setLevel(Level $level = null){
-		if($level !== null and $level->isClosed()){
-			throw new \InvalidArgumentException("Specified level has been unloaded and cannot be used");
-		}
-
-		$this->level = $level;
-		return $this;
-	}
-
-	/**
-	 * Checks if this object has a valid reference to a loaded Level
+	 * Returns whether this position has a valid target Dimension
 	 *
 	 * @return bool
 	 */
 	public function isValid(){
-		return $this->getLevel() instanceof Level;
+		return $this->getDimension() instanceof Dimension;
 	}
 
 	/**
@@ -100,23 +111,24 @@ class Position extends Vector3{
 	 * @throws LevelException
 	 */
 	public function getSide($side, $step = 1){
-		assert($this->isValid());
-
-		return Position::fromObject(parent::getSide($side, $step), $this->level);
+		return Position::fromObject(parent::getSide($side, $step), $this->getDimension());
 	}
 
 	public function __toString(){
-		return "Position(level=" . ($this->isValid() ? $this->getLevel()->getName() : "null") . ",x=" . $this->x . ",y=" . $this->y . ",z=" . $this->z . ")";
+		return "Position(" .
+			($this->isValid() ? "level=" . $this->dimension->getLevel()->getName() . ",dimension=" . $this->dimension->getSaveId()
+				: "level=null,dimension=null") .
+			",x=" . $this->x . ",y=" . $this->y . ",z=" . $this->z . ")";
 	}
 
 	/**
-	 * @param $x
-	 * @param $y
-	 * @param $z
+	 * @param int|float $x
+	 * @param int|float $y
+	 * @param int|float $z
 	 *
 	 * @return Position
 	 */
-	public function setComponents($x, $y, $z){
+	public function setComponents($x, $y, $z) : Position{
 		$this->x = $x;
 		$this->y = $y;
 		$this->z = $z;
